4567c2f1041e9ddd765bd0b69cca62ec8c0585df
Beatrice Georges-Lemire <ladybug2503@gmail.com>
2022-06-30 12:20:43 -0400

    added transitions and fixed up project section

--- app.js
+++ app.js
@@ -12,10 +12,14 @@ const observer = new IntersectionObserver(entries => {
 
 observer.observe(document.querySelector('.imgContainer'));
 
+const observerTwo = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+        if (entry.intersectionRatio > 0) {
+            entry.target.classList.add('textAnimation');
+            return;
+        }
+        document.querySelector('.aboutText').classList.remove('textAnimation');
+    });
+});
 
-
-// animation.init(() => {
-
-// });
-
-// animation.init();
+observerTwo.observe(document.querySelector('.aboutText'));
